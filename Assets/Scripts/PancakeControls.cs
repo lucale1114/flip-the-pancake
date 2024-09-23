cb268e93a50b807588ebc38aff2cfa8a2b395b88
Luca A. <92426198+lucale1114@users.noreply.github.com>
2024-09-23 13:11:01 +0200

    Created controllable pancake

--- Assets/Scripts/PancakeControls.cs
+++ Assets/Scripts/PancakeControls.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    readonly float PANCAKE_FLIP_SPEED = 1.6f;
+    readonly float PANCAKE_FLIP_SPEED = -5f;
+    readonly float HORIZONTAL_MOVESPEED = 105f;
+    private float rotationAcceleration = 0;
+
     void Start()
     {
         rb.AddForce(new Vector2(400, 500));
-        Time.timeScale = 0.5f;
+    }
+
+    private void MovementControls()
+    {
+        float axisHorizontalDirection = Input.GetAxis("Horizontal");
+        float axisVerticalDirection = Input.GetAxis("Vertical");
+
+        rotationAcceleration += (axisHorizontalDirection * PANCAKE_FLIP_SPEED) * Time.deltaTime;
+        Mathf.Clamp(rotationAcceleration, -1, 1);
+        rb.SetRotation(rb.rotation + rotationAcceleration);
+        rb.AddForce(new Vector2(axisHorizontalDirection * HORIZONTAL_MOVESPEED * Time.deltaTime, axisVerticalDirection /3f));
     }
 
     void Update()
     {
-        float rotateDirection = Input.GetAxis("Horizontal");
-        rb.SetRotation(rb.rotation + -rotateDirection * PANCAKE_FLIP_SPEED);
+        MovementControls();
+        if (rotationAcceleration > 0) 
+        {
+            rotationAcceleration -= 0.002f; 
+        }
+        else if (rotationAcceleration < 0)
+        {
+            rotationAcceleration += 0.002f;
+        }
     }
 }
