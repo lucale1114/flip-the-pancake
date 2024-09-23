36a93e3bd2b5ecc7aa41976a0523cedb429f036e
Luca.A <lucale1114@skola.goteborg.se>
2024-09-23 14:04:11 +0200

    added plate

--- Assets/Scripts/PancakeControls.cs
+++ Assets/Scripts/PancakeControls.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    readonly float PANCAKE_FLIP_SPEED = -5f;
-    readonly float HORIZONTAL_MOVESPEED = 105f;
+    readonly float PANCAKE_FLIP_SPEED = -4f;
+    readonly float HORIZONTAL_MOVESPEED = 155f;
+
+    private bool canMove = true;
     private float rotationAcceleration = 0;
 
     void Start()
@@ -20,13 +22,17 @@ public class NewBehaviourScript : MonoBehaviour
         float axisVerticalDirection = Input.GetAxis("Vertical");
 
         rotationAcceleration += (axisHorizontalDirection * PANCAKE_FLIP_SPEED) * Time.deltaTime;
-        Mathf.Clamp(rotationAcceleration, -1, 1);
+        rotationAcceleration = Mathf.Clamp(rotationAcceleration, -1.5f, 1.5f);
         rb.SetRotation(rb.rotation + rotationAcceleration);
         rb.AddForce(new Vector2(axisHorizontalDirection * HORIZONTAL_MOVESPEED * Time.deltaTime, axisVerticalDirection /3f));
     }
 
     void Update()
     {
+        if (!canMove)
+        {
+            return;
+        }
         MovementControls();
         if (rotationAcceleration > 0) 
         {
@@ -36,5 +42,63 @@ public class NewBehaviourScript : MonoBehaviour
         {
             rotationAcceleration += 0.002f;
         }
+    }
+
+    IEnumerator checkPancakeValid(Vector3 startPos, PlateScript plate)
+    {
+        yield return new WaitForSeconds(1);
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(startPos, -Vector2.up);
+
+        Debug.DrawLine(startPos, -Vector2.up, Color.red, 5);
+        if (raycastHit2D.collider.CompareTag("Pancake"))
+        {
+            print("is valid!");
+            if (plate)
+            {
+                plate.firstPancake = gameObject;
+            }
+            rb.bodyType = RigidbodyType2D.Static;
+
+        } else
+        {
+            print("not valid");
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator checkPancakeValid(Vector3 startPos)
+    {
+        yield return new WaitForSeconds(1);
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(startPos, Vector2.up * 2);
+        if (raycastHit2D.collider != null)
+        {
+            print("is valid!");
+            rb.bodyType = RigidbodyType2D.Static;
+
+        }
+        else
+        {
+            print("not valid");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Plate") && canMove)
+        {
+            PlateScript plateScript = other.gameObject.GetComponent<PlateScript>();
+            if (plateScript.firstPancake == null)
+            {
+                canMove = false;
+                StartCoroutine(checkPancakeValid(other.transform.position, plateScript));
+               
+            }
+        }
+    }
+
+    private void PlacedPancake()
+    {
+        
     }
 }
---
+++ Assets/Scripts/PlateScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateScript : MonoBehaviour
+{
+    public GameObject firstPancake = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
