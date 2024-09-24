5aa566c09a018911a0d9ec446c7e01081e40a117
Mojkan <elvinhillstrom@gmail.com>
2024-09-24 12:03:52 +0200

    Added the cat sprite and a script to it

---
+++ Assets/Scripts/CatPawController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPawController : MonoBehaviour
+{
+    [SerializeField] public int speed;
+    [SerializeField] float minPos;
+    [SerializeField] float maxPos;
+
+    Vector2 nextPos;
+
+    private void Start()
+    {
+        GenerateNewPos();
+    }
+
+    void Update()
+    {
+        if (nextPos.y > transform.position.y)
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if (nextPos.y < transform.position.y)
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+
+        Vector2 distance = nextPos - (Vector2)transform.position;
+        if (Vector2.SqrMagnitude(distance) < 0.01f)
+        {
+            GenerateNewPos();
+        }
+    }
+
+    void GenerateNewPos()
+    {
+        nextPos = new Vector2(transform.position.x, Random.Range(minPos, maxPos));
+    }
+}
